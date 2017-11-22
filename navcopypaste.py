535e5645618f3e644cee7aa78be7501d4bac0120
photek1944 <niels.cuelenaere@gmail.com>
2017-11-22 21:40:05 +0100

    Update navcopypaste.py

--- navcopypaste.py
+++ navcopypaste.py
@@ -3,13 +3,19 @@
 
 import pyperclip, pyautogui, time
 
+from win32api import GetKeyState
+from win32con import VK_CAPITAL
+
 var = pyperclip.paste()
 var = var.split('\r\n')
 
-print('Selecteer de bovenste cel van een kolom in Navision. Na 5 seconden begint het plakken')
-time.sleep(5)
+print('Selecteer de bovenste cel van een kolom in Navision. Na 3 seconden begint het plakken')
+time.sleep(3)
 
-pyautogui.PAUSE = 0.05
+pyautogui.PAUSE = 0.01
+
+if GetKeyState(VK_CAPITAL) == 1:
+    pyautogui.press('capslock')
 
 for i in var:
     print('Pasting: ' + i)
