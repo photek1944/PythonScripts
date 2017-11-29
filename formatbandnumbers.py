2bfe7a18e3c6f48e20df914707826ece6b7225f7
photek1944 <niels.cuelenaere@gmail.com>
2017-11-29 17:31:42 +0100

    Update formatbandnumbers.py

--- formatbandnumbers.py
+++ formatbandnumbers.py
@@ -17,8 +17,12 @@ ro1 = re.compile(r'(\d{7})/((\d{2})(\d{2})?)')
 string = ro1.sub(landcode + r'\2-\1', string)
 
 #17/1234567
-ro3 = re.compile(r'(\d{2})/(\d{7})')
+ro3 = re.compile(r'(?<!BE|NL)(\d{2})/(\d{7})')
 string = ro3.sub(landcode + r'\1-\2', string)
+
+#BE|NL17/1234567
+ro4 = re.compile(r'(BE|NL)(\d{2})/(\d{7})')
+string = ro4.sub(r'\1\2-\3', string)
 
 #2017/1234567
 #ro4 = re.compile(r'(\d{2})(\d{2})(-\d+)')
@@ -35,8 +39,12 @@ ro1 = re.compile(r'(\d{7})-((\d{2})(\d{2})?)')
 string = ro1.sub(landcode + r'\2-\1', string)
 
 #17-1234567
-ro3 = re.compile(r'(\d{2})(-\d{7})')
+ro3 = re.compile(r'(?<!BE|NL)(\d{2})(-\d{7})')
 string = ro3.sub(landcode + r'\1\2', string)
+
+#BE|NL17-1234567
+ro4 = re.compile(r'(BE|NL)(\d{2})-(\d{7})')
+string = ro4.sub(r'\1\2-\3', string)
 
 #XX17-1234567 - adds formatted band numbers to list of strings regardless of countrycode
 roX = re.compile(r'[a-zA-Z]{2}\d{2}-\d+')
