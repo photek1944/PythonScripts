ade9eb8abe366c7163be7c658fb6aa9b70ac9289
photek1944 <niels.cuelenaere@gmail.com>
2018-02-08 12:34:57 +0100

    Create windowsFilter.py

---
+++ windowsFilter.py
@@ -0,0 +1,20 @@
+#! python3
+# windowsfilter.py -- add the pipe " OR " character to the end of any string to create a filterstring for Microsoft Navision
+
+import pyperclip
+
+var = pyperclip.paste()
+var = var.replace('T-','')
+var = var.split('\r\n')
+
+if var[-1] == "":
+    del var[-1]
+
+for i in range(len(var)):
+    var[i] = str('*') + var[i]
+   
+var = ' OR '.join(var)
+var = var + ' -_'
+pyperclip.copy(var)
+
+print('Creating Windows Search Filter: ' + var)
